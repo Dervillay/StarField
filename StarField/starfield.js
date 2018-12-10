80d9037e8a7b9c018fb96cb9cd6ff41aefa67102
Dervillay <james.gascoigneburns7@gmail.com>
2018-12-10 19:02:56 +0000

    Turn starfield.js into a class and remove sliders

--- StarField/starfield.js
+++ StarField/starfield.js
@@ -1,185 +1,182 @@
-var speed = 1;
-var stars = []
-var planets = []
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    starDensity = createSlider(10, 2000);
-    starDensity.position(20, 20);
-    planetDensity = createSlider(10, 2000);
-    planetDensity.position(20, 60);
-    button = createButton("Generate Star Field");
-    button.position(30,90);
-    button.mousePressed(generate);
-}
-
-function generate() {
-  background(0);
-  noStroke();
-  stars = [];
-  planets = [];
+class StarField {
   
-  for (var i = 0; i < starDensity.value(); i++) {
-      stars[i] = new Star();
+  constructor(starDensity, planetDensity, maxSpeed) {
+    this.speed = 1;
+    this.maxSpeed = maxSpeed || 70;
+    this.stars = [];
+    this.starDensity = starDensity || 400;
+    this.planets = [];
+    this.planetDensity = planetDensity || 40;
+
+    background(0);
+    noStroke();
+    createCanvas(windowWidth, windowHeight);
+
+    for (var i = 0; i < this.starDensity; i++) {
+        stars[i] = new Star();
+    }
+
+    for (var i = 0; i < this.planetDensity; i++) {
+        planets[i] = new Planet();
+    }
+
   }
 
-  for (var i = 0; i < planetDensity.value(); i++) {
-      planets[i] = new Planet();
+
+  draw() {
+      fill(0, 100);
+      rect(0, 0, width, height);
+      translate(width / 2, height / 2);
+  		var mX = mouseX - width/2;
+  		var mY = mouseY - height/2;
+
+      for (var i = 0; i < this.stars.length; i++) {
+
+          this.stars[i].update();
+
+  				if ((Math.abs(this.stars[i].sx - mX) <= 5) && (Math.abs(this.stars[i].sy - mY) <= 5) && !mouseIsPressed) {
+  					this.stars[i].label();
+          }
+
+          this.stars[i].show();
+      }
+
+      for (var i = 0; i < this.planets.length; i++) {
+
+          this.planets[i].update();
+
+          if ((Math.abs(this.planets[i].sx - mX) <= 5) && (Math.abs(this.planets[i].sy - mY) <= 5) && !mouseIsPressed) {
+            this.planets[i].label();
+          }
+
+          this.planets[i].show();
+      }
+
+  		if (mouseIsPressed) {
+  				for (var i = 0; i < 8; i++) {
+  					if (speed < this.maxSpeed) {
+  						speed = speed + 0.1;
+  					}
+  				}
+  		} else if (mouseIsPressed === false) {
+  		    for (var i = 0; i < speed-0.4; i++) {
+  				    if (speed > 0) {
+  							     speed = speed - 0.1;
+  		        }
+  		    }
+  		}
   }
-}
 
-function draw() {
-    fill(0, 100);
-    rect(0, 0, width, height);
-    translate(width / 2, height / 2);
-		var mX = mouseX - width/2;
-		var mY = mouseY - height/2;
+  Star() {
+      this.x = random(-width / 2, width / 2);
+      this.y = random(-height / 2, height / 2);
+      this.z = random(width);
 
-    for (var i = 0; i < stars.length; i++) {
+  		this.makeName = function() {
+  			var name;
+  			var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  			var nums = "0123456789";
 
-        stars[i].update();
+  			for (var i = 0; i < 5; i++) {
+  				name += chars.charAt(Math.floor(Math.random() * chars.length));
+  			}
 
-				if ((Math.abs(stars[i].sx - mX) <= 5) && (Math.abs(stars[i].sy - mY) <= 5) && !mouseIsPressed) {
-					stars[i].label();
-        }
+  			name += " ";
 
-        stars[i].show();
-    }
+  			for (var i = 0; i < 2; i++) {
+  				name += nums.charAt(Math.floor(Math.random()* nums.length));
+  			}
 
-    for (var i = 0; i < planets.length; i++) {
+  			return name;
+  		}
 
-        planets[i].update();
+  		this.name = this.makeName().substring('undefined'.length);
 
-        if ((Math.abs(planets[i].sx - mX) <= 5) && (Math.abs(planets[i].sy - mY) <= 5) && !mouseIsPressed) {
-          planets[i].label();
-        }
+  		this.label = function() {
+  			fill(30, 30, 30);
+  			ellipse(this.sx, this.sy, 15, 15);
+  			rect(this.sx - 35, this.sy - 40, 70, 25, 3, 3, 3, 3);
+  			triangle(this.sx, this.sy - 10, this.sx - 7, this.sy - 16, this.sx + 7, this.sy - 16);
+  			fill(255);
+  			textSize(12);
+  			textFont("Arial");
+  			textAlign(CENTER);
+  			text(this.name, this.sx, this.sy - 23)
+  		}
 
-        planets[i].show();
-    }
+      this.update = function() {
+          this.z -= speed;
+          if (this.z < 1) {
+              this.z = width;
+              this.x = random(-width / 2, width / 2);
+              this.y = random(-height / 2, height / 2);
+          }
+      }
 
-		if (mouseIsPressed) {
-				for (var i = 0; i < 8; i++) {
-					if (speed < 70) {
-						speed = speed + 0.1;
-					}
-				}
-		} else if (mouseIsPressed === false) {
-		    for (var i = 0; i < speed-0.4; i++) {
-				    if (speed > 0) {
-							     speed = speed - 0.1;
-		        }
-		    }
-		}
-}
+      this.show = function() {
+          var sx = map(this.x / this.z, 0, 1, 0, width);
+          var sy = map(this.y / this.z, 0, 1, 0, height);
+          var r = map(this.z, 0, width, 5, 0);
 
-function Star() {
-    this.x = random(-width / 2, width / 2);
-    this.y = random(-height / 2, height / 2);
-    this.z = random(width);
+  				this.sx = sx;
+  				this.sy = sy;
 
-		this.makeName = function() {
-			var name;
-			var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-			var nums = "0123456789";
+  				fill(255);
+          ellipse(sx, sy, r, r);
+      }
+  }
 
-			for (var i = 0; i < 5; i++) {
-				name += chars.charAt(Math.floor(Math.random() * chars.length));
-			}
+  Planet() {
+  		this.x = random(-width / 2, width / 2);
+      this.y = random(-height / 2, height / 2);
+      this.z = random(width);
+      this.r = random(130);
+      this.g = random(130);
+      this.b = random(130);
 
-			name += " ";
+  		this.makeName = function() {
+  			var name;
+    		var gods = ["Zeus", "Hera", "Poseidon", "Hades", "Athena", "Apollo", "Artemis", "Aphrodite", "Hermes", "Ares" ];
+  			var numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
 
-			for (var i = 0; i < 2; i++) {
-				name += nums.charAt(Math.floor(Math.random()* nums.length));
-			}
+      	name = gods[Math.floor(Math.random() * gods.length)] + " " + numerals[Math.floor(Math.random() * numerals.length)];
+    		return name;
+  		}
 
-			return name;
-		}
+  		this.name = this.makeName();
 
-		this.name = this.makeName().substring('undefined'.length);
+  		this.label = function() {
+  			fill(30, 30, 30);
+  			ellipse(this.sx, this.sy, 15, 15);
+  			rect(this.sx - 35, this.sy - 40, 70, 25, 3, 3, 3, 3);
+  			triangle(this.sx, this.sy - 10, this.sx - 7, this.sy - 16, this.sx + 7, this.sy - 16);
+  			fill(255);
+  			textSize(12);
+  			textFont("Arial");
+  			textAlign(CENTER);
+  			text(this.name, this.sx, this.sy - 23)
+  		}
 
-		this.label = function() {
-			fill(30, 30, 30);
-			ellipse(this.sx, this.sy, 15, 15);
-			rect(this.sx - 35, this.sy - 40, 70, 25, 3, 3, 3, 3);
-			triangle(this.sx, this.sy - 10, this.sx - 7, this.sy - 16, this.sx + 7, this.sy - 16);
-			fill(255);
-			textSize(12);
-			textFont("Arial");
-			textAlign(CENTER);
-			text(this.name, this.sx, this.sy - 23)
-		}
+      this.update = function() {
+          this.z -= speed;
+          if (this.z < 1) {
+              this.z = width;
+              this.x = random(-width / 2, width / 2);
+              this.y = random(-height / 2, height / 2);
+          }
+      }
 
-    this.update = function() {
-        this.z -= speed;
-        if (this.z < 1) {
-            this.z = width;
-            this.x = random(-width / 2, width / 2);
-            this.y = random(-height / 2, height / 2);
-        }
-    }
+      this.show = function() {
+          var sx = map(this.x / this.z, 0, 1, 0, width);
+          var sy = map(this.y / this.z, 0, 1, 0, height);
+          var r = map(this.z, 0, width, 10, 0);
 
-    this.show = function() {
-        var sx = map(this.x / this.z, 0, 1, 0, width);
-        var sy = map(this.y / this.z, 0, 1, 0, height);
-        var r = map(this.z, 0, width, 5, 0);
+  				this.sx = sx;
+  				this.sy = sy;
 
-				this.sx = sx;
-				this.sy = sy;
-
-				fill(255);
-        ellipse(sx, sy, r, r);
-    }
-}
-
-function Planet() {
-		this.x = random(-width / 2, width / 2);
-    this.y = random(-height / 2, height / 2);
-    this.z = random(width);
-    this.r = random(130);
-    this.g = random(130);
-    this.b = random(130);
-
-		this.makeName = function() {
-			var name;
-  		var gods = ["Zeus", "Hera", "Poseidon", "Hades", "Athena", "Apollo", "Artemis", "Aphrodite", "Hermes", "Ares" ];
-			var numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
-
-    	name = gods[Math.floor(Math.random() * gods.length)] + " " + numerals[Math.floor(Math.random() * numerals.length)];
-  		return name;
-		}
-
-		this.name = this.makeName();
-
-		this.label = function() {
-			fill(30, 30, 30);
-			ellipse(this.sx, this.sy, 15, 15);
-			rect(this.sx - 35, this.sy - 40, 70, 25, 3, 3, 3, 3);
-			triangle(this.sx, this.sy - 10, this.sx - 7, this.sy - 16, this.sx + 7, this.sy - 16);
-			fill(255);
-			textSize(12);
-			textFont("Arial");
-			textAlign(CENTER);
-			text(this.name, this.sx, this.sy - 23)
-		}
-
-    this.update = function() {
-        this.z -= speed;
-        if (this.z < 1) {
-            this.z = width;
-            this.x = random(-width / 2, width / 2);
-            this.y = random(-height / 2, height / 2);
-        }
-    }
-
-    this.show = function() {
-        var sx = map(this.x / this.z, 0, 1, 0, width);
-        var sy = map(this.y / this.z, 0, 1, 0, height);
-        var r = map(this.z, 0, width, 10, 0);
-
-				this.sx = sx;
-				this.sy = sy;
-
-        fill(this.r, this.g, this.b);
-        ellipse(sx, sy, r, r);
-    }
+          fill(this.r, this.g, this.b);
+          ellipse(sx, sy, r, r);
+      }
+  }
 }
